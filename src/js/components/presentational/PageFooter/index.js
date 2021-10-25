03d6cf4a6ca50698abe644630a64feef52c84936
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2021-10-25 20:12:57 +1100

    Update PageFooter

--- src/js/components/presentational/PageFooter/index.js
+++ src/js/components/presentational/PageFooter/index.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PageFooter = () => (
-    <section className="cta-section text-center py-5 theme-bg-dark position-relative">
-        <div className="theme-bg-shapes-right"></div>
-        <div className="theme-bg-shapes-left"></div>
-        <div className="container">
-            <h3 className="mb-2 text-white mb-3">Launch Your Software Project Like A Pro</h3>
-            <div className="section-intro text-white mb-3 single-col-max mx-auto">Want to launch your software project
-                and start getting traction from your target users? Check out our premium <a className="text-white"
-                                                                                            href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/">Bootstrap
-                    5 startup template CoderPro</a>! It has everything you need to promote your product.
-            </div>
-            <div className="pt-3 text-center">
-                <a className="btn btn-light"
-                   href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/">Get
-                    CoderPro<i className="fas fa-arrow-alt-circle-right ml-2"></i></a>
-            </div>
-        </div>
-    </section>
+  <section className="cta-section text-center py-5 theme-bg-dark position-relative">
+    <div className="theme-bg-shapes-right"/>
+    <div className="theme-bg-shapes-left"/>
+    <div className="container">
+      <h3 className="mb-2 text-white mb-3">
+        Participate in Improving the Style Guide
+      </h3>
+      <div className="section-intro text-white mb-3 single-col-max mx-auto">
+        Want to help us improve the style guide? We always value new
+        perspectives and it is never too late to share your inputs. Click below
+        to take part in this journey.
+      </div>
+      <div className="pt-3 text-center">
+        <a
+          className="btn btn-light"
+          href="https://github.com/wearemav3rik/Mav3rik-Coding-Style-Guide"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Contribute <FontAwesomeIcon icon={['fas', 'arrow-alt-circle-right']} fixedWidth />
+        </a>
+      </div>
+    </div>
+  </section>
 );
 
 export default PageFooter;
