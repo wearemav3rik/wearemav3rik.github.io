d928b713f817812bc2d47a2b0fbf429c6889d054
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2021-10-25 20:17:13 +1100

    Add TableOfContents

---
+++ src/js/components/container/TableOfContents/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PageHeader from "../../presentational/PageHeader";
+import PageContent from "../../presentational/PageContent";
+import PageContentItem from "../../presentational/PageContentItem";
+import PageFooter from "../../presentational/PageFooter";
+import Footer from "../../presentational/Footer";
+
+import { contentsForApex } from "../../content/contentForApex";
+
+const TableOfContents = () => (
+  <React.Fragment>
+    <PageHeader />
+    <PageContent title="Apex">
+      {contentsForApex.map((content) => (
+        <PageContentItem
+          key={content.id}
+          title={content.title}
+          icon={content.icon}
+          href={`docs-page#${content.id}`}
+        >
+          {content.description}
+        </PageContentItem>
+      ))}
+    </PageContent>
+    <PageFooter />
+    <Footer />
+  </React.Fragment>
+);
+
+export default TableOfContents;
