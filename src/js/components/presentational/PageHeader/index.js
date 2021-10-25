21a4552bc6f327a2c56c2d4c413e298b3e2897bb
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2021-10-25 20:14:04 +1100

    Update PageHeader

--- src/js/components/presentational/PageHeader/index.js
+++ src/js/components/presentational/PageHeader/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PageHeader = () => (
     <div className="page-header theme-bg-dark py-5 text-center position-relative">
-        <div className="theme-bg-shapes-right"></div>
-        <div className="theme-bg-shapes-left"></div>
+        <div className="theme-bg-shapes-right"/>
+        <div className="theme-bg-shapes-left"/>
         <div className="container">
             <h1 className="page-heading single-col-max mx-auto">Coding Style Guide</h1>
             <div className="page-intro single-col-max mx-auto">
-                This style guide is the coding standards for the developers of Mav3rik.
+                The official coding standards for the developers of Mav3rik.
             </div>
             <div className="main-search-box pt-3 d-block mx-auto">
                 <form className="search-form w-100">
                     <input type="text" placeholder="Search the docs..." name="search"
                            className="form-control search-input"/>
-                    <button type="submit" className="btn search-btn" value="Search"><i
-                        className="fas fa-search"></i></button>
+                    <button type="submit" className="btn search-btn" value="Search">
+                        <FontAwesomeIcon icon={['fas','search']} fixedWidth />
+                    </button>
                 </form>
             </div>
         </div>
